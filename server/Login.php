7b59bfa4a023e0bd31f8ae571e0c54c472dbf713
stan5dard <stan5dard@naver.com>
2021-04-21 17:58:41 +0900

    server login

--- server/Login.php
+++ server/Login.php
@@ -5,7 +5,7 @@ $conn = mysqli_connect('localhost','root','rootroot','db_common');
 mysqli_query($conn, "set session character_set_connection=utf8");
 mysqli_query($conn, "set session character_set_results=utf8");
 mysqli_query($conn, "set session character_set_client=utf8");
-$query_cond = "SELECT COND FROM userinfo WHERE USERID=$userid";
+$query_cond = "SELECT COND FROM userinfo WHERE STUDENTID=$code";
 $result_cond = mysqli_query($conn, $query_cond);
 $cond = mysqli_fetch_array($result_cond)[0];
 $query = "SELECT * FROM userinfo WHERE STUDENTID=$code";
@@ -14,10 +14,10 @@ $result = mysqli_query($conn, $query);
 if($result){
     $row = mysqli_fetch_assoc($result);
     if($row["ALLOW_CONNECTION"]==1){
-        if($conn!=3){
+        if($cond!=3){
             echo $row["USERID"]."/_/".$row["COND"]."/_/".$row["COND3_FIRSTLAUNCH"]."/_/";
         }
-        else if($conn==3){
+        else if($cond==3){
             $query_get_user_name = "SELECT USERNAME FROM userinfo WHERE STUDENTID=$code";
             $result_get_user_name = mysqli_query($conn, $query_get_user_name);
             $user_name = mysqli_fetch_row($result_get_user_name)[0];
--- server/test2.php
+++ server/test2.php
@@ -5,14 +5,24 @@ $conn = mysqli_connect('localhost','root','rootroot','db_common');
 mysqli_query($conn, "set session character_set_connection=utf8");
 mysqli_query($conn, "set session character_set_results=utf8");
 mysqli_query($conn, "set session character_set_client=utf8");
-
+$query_cond = "SELECT COND FROM userinfo WHERE STUDENTID=$code";
+$result_cond = mysqli_query($conn, $query_cond);
+$cond = mysqli_fetch_array($result_cond)[0];
 $query = "SELECT * FROM userinfo WHERE STUDENTID=$code";
 $result = mysqli_query($conn, $query);
 
 if($result){
     $row = mysqli_fetch_assoc($result);
     if($row["ALLOW_CONNECTION"]==1){
-        echo $row["USERID"]."/_/".$row["COND"]."/_/".$row["COND3_FIRSTLAUNCH"]."/_/";
+        if($cond!=3){
+            echo $row["USERID"]."/_/".$row["COND"]."/_/".$row["COND3_FIRSTLAUNCH"]."/_/";
+        }
+        else if($cond==3){
+            $query_get_user_name = "SELECT USERNAME FROM userinfo WHERE STUDENTID=$code";
+            $result_get_user_name = mysqli_query($conn, $query_get_user_name);
+            $user_name = mysqli_fetch_row($result_get_user_name)[0];
+            echo $user_name."/_/".$row["COND"]."/_/".$row["COND3_FIRSTLAUNCH"]."/_/";
+        }
     }
     else{
         echo "NOT ALLOWED/_/";
